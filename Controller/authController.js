a36e8e16c5c1bdc88564a3b90b163f4ff525713a
Sahitya_Singh <sahityasingh1211@gmail.com>
2025-02-22 20:55:23 +0530

    vox removed

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,16 +1,6 @@
 const admin = require('../config/firebaseConfig');
 const jwt = require('jsonwebtoken');
 const Business = require('../models/Business');
-const axios = require("axios");
-
-const VoximplantApiClient = require('@voximplant/apiclient-nodejs').default;
-
-const path = require('path');
-// Twilio credentials
-
-const credentialsPath = path.join(__dirname, '..', 'config', 'a1b016b8-e589-4914-9760-8ccc2e089f4c_private.json');
-const client = new VoximplantApiClient(credentialsPath);
-
 
 // Login Handler
 exports.login = async (req, res) => {
@@ -75,52 +65,6 @@ exports.verifyOtp = async (req, res) => {
     }
   };
 
-  function generateSecurePassword() {
-    const length = 12;
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-    let password = "";
-    for (let i = 0; i < length; i++) {
-      password += charset.charAt(Math.floor(Math.random() * charset.length));
-    }
-    return password;
-  }
-
-  function generateValidUserName(name) {
-    // Convert to lowercase, remove non-Latin characters, and replace spaces with hyphens
-    let sanitized = name.toLowerCase().replace(/[^a-z0-9-]/g, '');
-
-    // Ensure the username starts with a letter or digit
-    if (!/^[a-z0-9]/.test(sanitized)) {
-        sanitized = 'user-' + sanitized;
-    }
-
-    // Ensure a minimum length of 3 characters
-    if (sanitized.length < 3) {
-        sanitized = sanitized.padEnd(3, 'x');
-    }
-
-    return sanitized;
-}
-
-async function createVoximplantUser(businessName, ownerName, phoneNumber) {
-  try {
-
-      const params = {
-          userName: businessName,
-          userDisplayName: ownerName,
-          userPassword: phoneNumber,
-          applicationName: 'kb.krish.n2.voximplant.com' // Replace this with your actual application ID from Voximplant Control Panel
-      };
-      console.log("Inside create: ", phoneNumber, businessName);
-      const result = await client.Users.addUser(params);
-      return result;
-  } catch (error) {
-      console.error('Error creating Voximplant user:', error);
-      throw error;
-  }
-}
-
-  
 // Signup Handlerc
 exports.signup = async (req, res) => {
     console.log("INSIDE SIGNUP");
@@ -176,14 +120,6 @@ exports.signup = async (req, res) => {
         { expiresIn: "1h" }
       );
 
-      try {
-        const output = await createVoximplantUser(newBusiness.businessName, newBusiness.ownerName, newBusiness.contact);
-        console.log("User successfully registered: ", output);
-    } catch (voxError) {
-        console.error('Error creating Voximplant user:', voxError);
-        // Decide if you want to fail the verification or continue despite the error
-    }
-      
       res.status(201).json({ message: "Signup successful", token });
     } catch (err) {
       console.error("Error during signup:", err);
